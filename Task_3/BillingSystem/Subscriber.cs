4fa9c76b922ccb69b1cf35bdce07655392a249b6
Dima <LukashevichDV@mail.ru>
2017-05-20 00:46:08 +0300

    First

---
+++ Task_3/BillingSystem/Subscriber.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_3.BillingSystem
+{
+    public class Subscriber
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Money { get; private set; }
+
+
+        public Subscriber(string firstname, string lastName)
+        {
+            FirstName = firstname;
+            LastName = lastName;
+            Money = 50;
+        }
+
+
+        public void AddMoney(int money)
+        {
+            Money += money;
+        }
+
+
+        public void WithDraw(int money)
+        {
+            Money -= money;
+        }
+    }
+}
